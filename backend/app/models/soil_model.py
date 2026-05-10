0180ec304809670b0d07bff1cd607aea0591d7bd
Kobiram11 <thiyagarajahkobiram@gmail.com>
2026-05-10 13:43:56 +0530

    Revert "04-05-2026 - Develop soil_model database"
    This reverts commit c8d1642465df67afe6fffc12f04f575b530802f0.

--- backend/app/models/soil_model.py
+++
@@ -1,9 +0,0 @@
-class FertilizerMobileInput(BaseModel):
-    moisture: float
-    temp: float
-    ec: float
-    ph: float
-    nitrogen: float
-    phosphorus: float
-    potassium: float
-    tree_age: float
