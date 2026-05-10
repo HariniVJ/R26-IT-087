c8d1642465df67afe6fffc12f04f575b530802f0
Kobiram11 <thiyagarajahkobiram@gmail.com>
2026-05-10 13:42:08 +0530

    04-05-2026 - Develop soil_model database

---
+++ backend/app/models/soil_model.py
@@ -0,0 +1,9 @@
+class FertilizerMobileInput(BaseModel):
+    moisture: float
+    temp: float
+    ec: float
+    ph: float
+    nitrogen: float
+    phosphorus: float
+    potassium: float
+    tree_age: float
